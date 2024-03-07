570e92e307728cf4c909081a078d7a8c05de8c66
Yash Khobragade <89641813+yashkhobragade@users.noreply.github.com>
2024-03-07 12:23:30 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(score);
+let toNumber = Number(score)
+// console.log(typeof(toNumber));
+// console.log(toNumber);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
