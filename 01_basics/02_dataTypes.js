f87ef324c500fada7ee8017e3dc19ecf6d9bbaed
Yash Khobragade <89641813+yashkhobragade@users.noreply.github.com>
2024-03-07 10:49:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treats  all JS code as a newer version
+
+//alert(3+3)we are not using browser ,using node
+console.log(2+2); console.log("yash");// avoid these code readability should be high instead write tin separate lines
+
+let name = "yash"
+let age = 21
+let isLoggedIn = true
+//number => 2 power53
+//bigint
+//boolean
+//strings = ""
+//null => standalone value
+//undefined
+//symbol => unique
+console.log(typeof "yash")
+console.log(typeof age)
+console.log(typeof name)
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
