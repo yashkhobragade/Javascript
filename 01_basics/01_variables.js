b7d8591ab62b9e74a16990b92435f54802a228c0
Yash Khobragade <89641813+yashkhobragade@users.noreply.github.com>
2024-03-07 10:49:01 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12233
+let accountEmail = "yash@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+let accountState;
+accountEmail = "hchc@fk.com"
+accountPassword = "1321432"
+accountCity = "bangalore"
+// accountId = 22
+console.log(accountId);
+console.table([accountId,accountPassword,accountCity,accountState])
