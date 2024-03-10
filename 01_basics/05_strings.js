cd2bcb2fb9ddc67978071964139cb3939a4caee0
Yash Khobragade <89641813+yashkhobragade@users.noreply.github.com>
2024-03-10 18:10:20 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "yash";
+const age = 22;
+
+//console.log(name + age + "."); // dont use this
+console.log(`My name is ${name} and my age is ${age}.`);// instead use this
+
+const gameName = new String ( 'yash')
+// console. log (gameName [0]) ;
+// console. log (gameName._proto_);
+// console. log (gameName. length) ;
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt('3'))
+const newString = gameName. substring(0, 4)
+console. log (newString);
+
+const anotherString = gameName. slice (-8, 4)
+console. log (anotherString) ;
+const newStringOne = "       audi         "
+console. log (newStringOne);
+console. log (newStringOne. trim ());
