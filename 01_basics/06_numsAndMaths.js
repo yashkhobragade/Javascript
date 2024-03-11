ee499cd30741e8a53acf3df6cec28821bed8634c
Yash Khobragade <89641813+yashkhobragade@users.noreply.github.com>
2024-03-11 07:29:58 +0000

    maths and dates

---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,19 @@
+const score = 300
+// console.log(score);
+
+const balance = new Number(210)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const num1 = 210.9822
+// console.log(num1.toPrecision(4));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ................ Maths ................
+
+console.log(Math);
+console.log(Math.abs(-1));
---
+++ 01_basics/07_dates.js
@@ -0,0 +1,21 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof(myDate));
+
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toLocaleDateString());
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14" )
+let myCreatedDate = new Date("01-14-2023" )
+// console. Log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp) ;
