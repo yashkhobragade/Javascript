c6ecccec391ffe91081784e46056670ad7fde1ec
Yash Khobragade <89641813+yashkhobragade@users.noreply.github.com>
2024-03-12 19:19:25 +0530

    Add files via upload
    arrays

---
+++ 01_arrays.js
@@ -0,0 +1,38 @@
+const myArr = [1,2,3,4,5,6]
+const arr2 = ["yash", "abcd"]
+const arr3 = new Array(1,2,3,4)
+
+// console.log(myArr);
+// console.log(arr2);
+// console.log(arr3);
+
+
+//*********Array methods**********
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(4));
+
+// const newarr= myArr.join()
+// console.log(newarr);
+// console.log(typeof newarr);
+
+//******* slics and splice */
+
+// console. log("A", myArr);
+
+// const myn1 = myArr. slice (1, 3)
+// console. log (myn1) ;
+// console. log("B ", myArr);
+
+// const myn2 = myArr. splice (1, 3)
+// console. log("C", myArr);
+// console. log(myn2) ;
+
+
